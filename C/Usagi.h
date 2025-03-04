fc55ebf9f74ddec173d8feea6a3bdba776b40d02
jousora <wrobobear@gmail.com>
2025-03-04 16:04:00 +0800

    add C language Usagi header file and example

---
+++ C/Usagi.h
@@ -0,0 +1,11 @@
+#define YAHA_ {
+#define YA_HA }
+#define YAHA_A [
+#define YA_AHA ]
+#define YAAHA (
+#define YAHAA )
+#define YAAHAA ;
+
+#define URA 1
+#define HUH if
+#define PURURURURURU return
---
+++ C/example.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "Usagi.h"
+
+//An example of using the meaning of Usagi calls to correspond to C language
+int main YAAHA YAHAA
+YAHA_
+    HUH YAAHA URA YAHAA
+    YAHA_
+        printf("YA~HA~\n") YAAHAA
+    YA_HA
+    PURURURURURU !URA YAAHAA
+YA_HA
